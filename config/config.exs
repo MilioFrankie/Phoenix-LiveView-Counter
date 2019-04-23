5a967fe3d16691f417e04c093e2ab4bbfef483a4
Emilio <em.francomartinez@gmail.com>
2019-04-23 17:22:30 -0600

    LiveView setup

---
+++ config/config.exs
@@ -0,0 +1,31 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+# Configures the endpoint
+config :live_view_counter, LiveViewCounterWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "Xed7+X8lp6YMlM504F3fR6baQPmT6wY7sxhlddcYcy1SxR4ksWsonaRjtdsJLxya",
+  render_errors: [view: LiveViewCounterWeb.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: LiveViewCounter.PubSub, adapter: Phoenix.PubSub.PG2],
+  live_view: [signing_salt: "wFTxY58yWx1rO5J4/wwg9kKI7g8JH+nW"]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Render leex files with the liveView Engine
+config :phoenix, template_engines: [leex: Phoenix.LiveView.Engine]
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
